a9d2baec014f23f0af0d868e0201720760234c7d
jameskomo <james.komoh@gmail.com>
2019-03-06 10:05:30 +0300

    Business Logic prototypes and constructors done

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ var Lenders = function(name, interest, lender) {
 };
 
 var barclays = new Lenders("Barclays", 18, "Bank");
-var kcb =  new Lenders("KCB", 13, "Bank");
+var kcb = new Lenders("KCB", 13, "Bank");
 var equity = new Lenders("Equity", 14, "Bank");
 var coop = new Lenders("Cooperative", 15, "Bank");
 var mshwari = new Lenders("Mshwari", 7.5, "Micro Lender");
@@ -26,14 +26,14 @@ Lenders.prototype.lenderName = function() {
 function loan(amount, term, facility) {
   var institution = facility.lenderCheck();
 
-  if(institution == "Bank"){
-    var interestAmount = amount * facility.interest/100 * term/12;
+  if (institution == "Bank") {
+    var interestAmount = amount * facility.interest / 100 * term / 12;
     var totalLoan = amount + interestAmount;
     return totalLoan;
   }
 
-  if(institution == "Micro Lender") {
-    var interestMicro = amount * facility.interest/100;
+  if (institution == "Micro Lender") {
+    var interestMicro = amount * facility.interest / 100;
     var totalMicroLoan = amount + interestMicro;
     return totalMicroLoan;
   }
@@ -45,30 +45,30 @@ function appFee(amount, lender) {
   var appFees = 0;
 
   if (lenderType == "Bank") {
-    if (lenderInterest >= 10 && lenderInterest <= 12){
-      appFees = 0.05*amount;
+    if (lenderInterest >= 10 && lenderInterest <= 12) {
+      appFees = 0.05 * amount;
       return appFees;
-    } else if (lenderInterest >= 13 && lenderInterest <= 14){
-      appFees = 0.10*amount;
+    } else if (lenderInterest >= 13 && lenderInterest <= 14) {
+      appFees = 0.10 * amount;
       return appFees;
-    } else if (lenderInterest >= 15 && lenderInterest <= 17){
-      appFees = 0.12*amount;
+    } else if (lenderInterest >= 15 && lenderInterest <= 17) {
+      appFees = 0.12 * amount;
       return appFees;
-    } else if (lenderInterest >= 18 && lenderInterest <= 20){
-      appFees = 0.17*amount;
+    } else if (lenderInterest >= 18 && lenderInterest <= 20) {
+      appFees = 0.17 * amount;
       return appFees;
     }
   }
 
-    var lenderName = lender.lenderName();
+  var lenderName = lender.lenderName();
 
-    if (lenderType == "Micro Lender"){
-      if (lenderName == "Tala") {
-        appFees = 100;
-        return appFees;
-      } else if (lenderName == "Mshwari") {
-        appFees = 150;
-        return appFees;
-      }
+  if (lenderType == "Micro Lender") {
+    if (lenderName == "Tala") {
+      appFees = 100;
+      return appFees;
+    } else if (lenderName == "Mshwari") {
+      appFees = 150;
+      return appFees;
     }
   }
+}
