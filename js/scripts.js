90620b85432d42f71bf6280f307c620827b9f5cd
spgdaman <gatheru.simon432@gmail.com>
2019-03-06 09:46:26 +0300

    Add lenders

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,10 @@ var Lenders = function(name, interest, lender) {
   this.interest = interest;
   this.lender = lender;
 };
+
+var barclays = new Lenders("Barclays", 18, "Bank");
+var kcb =  new Lenders("KCB", 13, "Bank");
+var equity = new Lenders("Equity", 14, "Bank");
+var coop = new Lenders("Cooperative", 15, "Bank");
+var mshwari = new Lenders("Mshwari", 7.5, "Micro Lender");
+var tala = new Lenders("Tala", 15, "Micro Lender");
