571af3a1b238cddf87c4e9f97560a6f43acfda0f
spgdaman <gatheru.simon432@gmail.com>
2019-03-07 09:11:46 +0300

    Refactor loan calculator using functional programming

--- js/scripts.js
+++ js/scripts.js
@@ -1,102 +1,107 @@
-// jshint esversion: 6
-var Lenders = function(name, interest, lender) {
-  this.name = name;
-  this.interest = interest;
-  this.lender = lender;
-};
-
-var barclays = new Lenders("Barclays", 18, "Bank");
-var kcb = new Lenders("KCB", 13, "Bank");
-var equity = new Lenders("Equity", 14, "Bank");
-var coop = new Lenders("Cooperative", 15, "Bank");
-var mshwari = new Lenders("Mshwari", 7.5, "Micro Lender");
-var tala = new Lenders("Tala", 15, "Micro Lender");
-
-Lenders.prototype.lenderCheck = function() {
-  return this.lender;
-};
-
-Lenders.prototype.lenderInterest = function() {
-  return this.interest;
-};
-
-Lenders.prototype.lenderName = function() {
-  return this.name;
-};
-
-function loan(amount, term, facility) {
-  var institution = facility.lenderCheck();
-
-  if (institution == "Bank") {
-    var interestAmount = amount * facility.interest / 100 * term / 12;
-    var totalLoan = amount + interestAmount;
+function bankLoan(amount, term, interest) {
+    var totalLoan = amount + (amount * interest / 100 * term / 12);
     return totalLoan;
-  }
-
-  if (institution == "Micro Lender") {
-    var interestMicro = amount * facility.interest / 100;
-    var totalMicroLoan = amount + interestMicro;
-    return totalMicroLoan;
-  }
 }
 
-function appFee(amount, lender) {
-  var lenderType = lender.lenderCheck();
-  var lenderInterest = lender.lenderInterest();
+function microLoan(amount, interest) {
+      var totalMicroLoan = amount + (amount * interest / 100);
+      return totalMicroLoan;
+}
+
+function appFee(amount, interest) {
   var appFees = 0;
 
-  if (lenderType == "Bank") {
-    if (lenderInterest >= 10 && lenderInterest <= 12) {
+    if (interest >= 10 && interest <= 12) {
       appFees = 0.05 * amount;
       return appFees;
-    } else if (lenderInterest >= 13 && lenderInterest <= 14) {
+    } else if (interest >= 13 && interest <= 14) {
       appFees = 0.10 * amount;
       return appFees;
-    } else if (lenderInterest >= 15 && lenderInterest <= 17) {
+    } else if (interest >= 15 && interest <= 17) {
       appFees = 0.12 * amount;
       return appFees;
-    } else if (lenderInterest >= 18 && lenderInterest <= 20) {
+    } else if (interest >= 18 && interest <= 20) {
       appFees = 0.17 * amount;
       return appFees;
-    }
-  }
-  else{
+    } else {
     return appFees;
-  }
+    }
+
 }
 
-function cheapestLender(amount, term, facility) {
-  var institution = facility.lenderCheck();
-  var cheapLender = facility.lenderInterest();
-  let bankInterest = 0;
-  let totalBankLoan = 0;
-  let microinterest = 0;
-  let totalMicroLoan = 0;
 
-  if (institution == "Bank" && cheapLender < 14) {
-    bankInterestt = amount * facility.interest / 100 * term / 12;
-    totalBankLoan = amount + bankInterest;
-    return totalBankLoan;
-  }
+$(document).ready(function() {
+  $('#check').click(function() {
+    event.preventDefault();
+    var loanAmount = parseInt($('#loan-amount').val());
+    var loanTerm = parseInt($("#loan-term").val());
+    var interestRate = parseFloat($("#lender-list option:selected").val());
+    var lenderName = $("#lender-list option:selected").text();
+    console.log(loanAmount);
+    console.log(loanTerm);
+    console.log(interestRate);
+    console.log(lenderName);
 
-  if (institution == "Micro Lender" && cheapLender < 10) {
-    microinterest = amount * facility.interest / 100;
-    totalMicroLoan = amount + microinterest;
-    return totalMicroLoan;
-  }
-}
+    if(lenderName === "Mshwari" || lenderName === "Tala") {
+      console.log(microLoan(loanAmount, interestRate));
+    } else {
+      var loanPlusFees = bankLoan(loanAmount, loanTerm, interestRate) + appFee(loanAmount, interestRate);
+      console.log(loanPlusFees);
+    }
 
-$(document).ready(function () {
-  $("#check").click(function(e) {
-    e.preventDefault();
 
-    var loanAmount = parseInt($("input#loan-amount").val());
-    var loanTerm = parseInt($("input#loan-term").val());
-    var lenderList = $("select#lender-list option: selected").val();
+    // var loanRequested = ;
+    // console.log(loanRequested);
+    // console.log(appFee(10000, kcb));
 
-    var requestedLoan = loan(loanAmount, loanTerm, lenderList);
-    var applicationFee = appfee(loanAmount, lenderList);
 
-    var potentialLoan = requestedLoan + applicationFee;
+
+    // console.log(requestedLoan);
+    // var applicationFee = appFee(loanAmount, lenderList);
+    // var potentialLoan = requestedLoan + applicationFee;
+    // var monthlyPayments = potentialLoan / 12;
+
+
   });
 });
+
+
+// function loan(amount, term, facility) {
+//   var institution = facility.lender;
+//   let totalLoan = [];
+//   let totalMicroLoan = [];
+//   if (institution == "Bank") {
+//     var interestAmount = amount * facility.interest / 100 * term / 12;
+//     totalLoan.push(amount + interestAmount);
+//   }
+//   return totalLoan;
+//
+//   if (institution == "Micro Lender") {
+//     var interestMicro = amount * facility.interest / 100;
+//     totalMicroLoan.push(amount + interestMicro);
+//   }
+//   return totalMicroLoan;
+// }
+
+// function cheapestLender(amount, term, facility) {
+//   var institution = facility.lender;
+//   var cheapLender = facility.interest;
+//   let bankInterest = 0;
+//   let totalBankLoan = 0;
+//   let microinterest = 0;
+//   let totalMicroLoan = 0;
+//
+//   if (institution == "Bank" && cheapLender < 14) {
+//     bankInterest = amount * facility.interest / 100 * term / 12;
+//     totalBankLoan = amount + bankInterest;
+//     return totalBankLoan;
+//   }
+//
+//   if (institution == "Micro Lender" && cheapLender < 10) {
+//     microinterest = amount * facility.interest / 100;
+//     totalMicroLoan = amount + microinterest;
+//     return totalMicroLoan;
+//   }
+// }
+
+// console.log(loan(10000,12,equity));
