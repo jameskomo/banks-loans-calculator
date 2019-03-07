682a9ac226354cced0c4a1f6104c5f26d6ac8829
spgdaman <gatheru.simon432@gmail.com>
2019-03-07 15:59:26 +0300

    Global code refactoring

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,31 @@
 function bankLoan(amount, term, interest) {
-    var totalLoan = amount + (amount * interest / 100 ) * term / 12;
-    return totalLoan;
+  var totalLoan = amount + (amount * interest / 100) * term / 12;
+  return totalLoan;
 }
 
 function microLoan(amount, interest) {
-      var totalMicroLoan = amount + (amount * interest / 100);
-      return totalMicroLoan;
+  var totalMicroLoan = amount + (amount * interest / 100);
+  return totalMicroLoan;
 }
 
 function appFee(amount, interest) {
   var appFees = 0;
 
-    if (interest >= 10 && interest <= 12) {
-      appFees = 0.05 * amount;
-      return appFees;
-    } else if (interest >= 13 && interest <= 14) {
-      appFees = 0.10 * amount;
-      return appFees;
-    } else if (interest >= 15 && interest <= 17) {
-      appFees = 0.12 * amount;
-      return appFees;
-    } else if (interest >= 18 && interest <= 20) {
-      appFees = 0.17 * amount;
-      return appFees;
-    } else {
+  if (interest >= 10 && interest <= 12) {
+    appFees = 0.05 * amount;
     return appFees;
-    }
+  } else if (interest >= 13 && interest <= 14) {
+    appFees = 0.10 * amount;
+    return appFees;
+  } else if (interest >= 15 && interest <= 17) {
+    appFees = 0.12 * amount;
+    return appFees;
+  } else if (interest >= 18 && interest <= 20) {
+    appFees = 0.17 * amount;
+    return appFees;
+  } else {
+    return appFees;
+  }
 }
 
 
@@ -43,10 +43,10 @@ $(document).ready(function() {
     console.log(interestRate);
     console.log(lenderName);
 
-    if(lenderName === "Mshwari" || lenderName === "Tala") {
+    if (lenderName === "Mshwari" || lenderName === "Tala") {
       var totalAmountPayable = Math.round(microLoan(loanAmount, interestRate));
       var interestPayable = totalAmountPayable - loanAmount;
-      var monthlyPayments = Math.round(totalAmountPayable/loanTerm);
+      var monthlyPayments = Math.round(totalAmountPayable / loanTerm);
 
       $("#loanProvider").text(lenderName);
       $("#requestedAmount").text(loanAmount);
@@ -57,27 +57,30 @@ $(document).ready(function() {
       $("#applicationCost").text(0);
       $("#annualInterest").text(totalAmountPayable);
 
-      $("#compareLoans").click(function() {
-        if(lenderName === "Mshwari") {
-
-        $("#loanProvider2").text(lenderName);
-        $("#requestedAmount2").text(loanAmount);
-        $("#loanTerm2").text(loanTerm);
-        $("#monthlyPayments2").text(monthlyPayments);
-        $("#totalInterest2").text(interestPayable);
-        $("#totalAmountPayable2").text(0);
-        $("#applicationCost2").text(0);
-        $("#annualInterest2").text(totalAmountPayable);
-
+      var cheaperMicro = function(name) {
+        if (name == "Mshwari") {
+          $("#loanProvider2").text(lenderName);
+          $("#requestedAmount2").text(loanAmount);
+          $("#loanTerm2").text(loanTerm);
+          $("#monthlyPayments2").text(monthlyPayments);
+          $("#totalInterest2").text(interestPayable);
+          $("#totalAmountPayable2").text(0);
+          $("#applicationCost2").text(0);
+          $("#annualInterest2").text(totalAmountPayable);
+          $("#loanResults").show();
+          $("#loanResults2").show();
+          $('#clearTable').show();
         }
+      };
 
-      });
+      cheaperMicro(Mshwari);
     }
-    if (lenderName !== "Mshwari" || lenderName !== "Tala"){
+
+    if (lenderName !== "Mshwari" || lenderName !== "Tala") {
       var appFees = Math.round(appFee(loanAmount, interestRate));
       var loanPlusInterest = bankLoan(loanAmount, loanTerm, interestRate);
       var loanPlusFees = loanPlusInterest + appFees;
-      var bankMonthlyPayments = Math.round(loanPlusFees/loanTerm);
+      var bankMonthlyPayments = Math.round(loanPlusFees / loanTerm);
       var bankInterestPayable = loanPlusFees - loanAmount;
       console.log(loanPlusFees);
 
@@ -89,9 +92,12 @@ $(document).ready(function() {
       $("#totalAmountPayable").text(interestRate + "%");
       $("#applicationCost").text(appFees);
       $("#annualInterest").text(loanPlusFees);
+      $('#loanResults').show();
+      $('#loanResults2').show();
+      $('#clearTable').show();
 
-      $("#compareLoans").click(function() {
-        if(lenderName === "KCB") {
+      var cheaperBank = function(name) {
+        if (lenderName == "KCB") {
           $("#loanProvider2").text(lenderName);
           $("#requestedAmount2").text(loanAmount);
           $("#loanTerm2").text(loanTerm);
@@ -101,8 +107,9 @@ $(document).ready(function() {
           $("#applicationCost2").text(appFees);
           $("#annualInterest2").text(loanPlusFees);
         }
-      });
+      };
 
+      cheaperBank(KCB);
     }
 
 
@@ -125,9 +132,6 @@ $(document).ready(function() {
       $("#applicationCost2").text("");
       $("#annualInterest2").text("");
     });
-
-
-
 
   });
 });
