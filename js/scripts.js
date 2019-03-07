f2ca2c8ad0ecb4fe6912ad3a5fa16ffbc3ffaadd
spgdaman <gatheru.simon432@gmail.com>
2019-03-07 11:36:46 +0300

    Push values to html on loan information

--- js/scripts.js
+++ js/scripts.js
@@ -36,73 +36,45 @@ $(document).ready(function() {
     var loanTerm = parseInt($("#loan-term").val());
     var interestRate = parseFloat($("#lender-list option:selected").val());
     var lenderName = $("#lender-list option:selected").text();
+
+
     console.log(loanAmount);
     console.log(loanTerm);
     console.log(interestRate);
     console.log(lenderName);
 
     if(lenderName === "Mshwari" || lenderName === "Tala") {
-      console.log(Math.round(microLoan(loanAmount, interestRate)));
+      var totalAmountPayable = Math.round(microLoan(loanAmount, interestRate));
+      var interestPayable = totalAmountPayable - loanAmount;
+      var monthlyPayments = Math.round(totalAmountPayable/loanTerm);
+
+      $("#monthly-payments").text(monthlyPayments);
+
+      // alert(totalAmountPayable);
     } else {
       var loanPlusFees = Math.round(bankLoan(loanAmount, loanTerm, interestRate) + appFee(loanAmount, interestRate));
       console.log(loanPlusFees);
+
+
     }
+    $('#tableView').click(function() {
+      var insertInformation = function () {
+        var table = document.getElementById("loanResults");
+        var row = table.insertRow(0);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        var cell4 = row.insertCell(3);
+
+        cell1.innerHTML = "loanAmount";
+        cell2.innerHTML = "totalAmountPayable";
+        cell3.innerHTML = "interestPayable";
+        cell4.innerHTML = "loanTerm";
+    };
+  });
 
 
-
-
-    // var loanRequested = ;
-    // console.log(loanRequested);
-    // console.log(appFee(10000, kcb));
-
-
-
-    // console.log(requestedLoan);
-    // var applicationFee = appFee(loanAmount, lenderList);
-    // var potentialLoan = requestedLoan + applicationFee;
-    // var monthlyPayments = potentialLoan / 12;
 
 
   });
 });
-
-
-// function loan(amount, term, facility) {
-//   var institution = facility.lender;
-//   let totalLoan = [];
-//   let totalMicroLoan = [];
-//   if (institution == "Bank") {
-//     var interestAmount = amount * facility.interest / 100 * term / 12;
-//     totalLoan.push(amount + interestAmount);
-//   }
-//   return totalLoan;
-//
-//   if (institution == "Micro Lender") {
-//     var interestMicro = amount * facility.interest / 100;
-//     totalMicroLoan.push(amount + interestMicro);
-//   }
-//   return totalMicroLoan;
-// }
-
-// function cheapestLender(amount, term, facility) {
-//   var institution = facility.lender;
-//   var cheapLender = facility.interest;
-//   let bankInterest = 0;
-//   let totalBankLoan = 0;
-//   let microinterest = 0;
-//   let totalMicroLoan = 0;
-//
-//   if (institution == "Bank" && cheapLender < 14) {
-//     bankInterest = amount * facility.interest / 100 * term / 12;
-//     totalBankLoan = amount + bankInterest;
-//     return totalBankLoan;
-//   }
-//
-//   if (institution == "Micro Lender" && cheapLender < 10) {
-//     microinterest = amount * facility.interest / 100;
-//     totalMicroLoan = amount + microinterest;
-//     return totalMicroLoan;
-//   }
-// }
-
-// console.log(loan(10000,12,equity));
